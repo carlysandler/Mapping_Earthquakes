6eca5432a52a3f84983fdb7fcc6e76c7ccff86bc
Carly Sandler <carly.sandler@gmail.com>
2021-05-13 09:12:05 -0400

    adding style to earthquake data

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -28,11 +28,6 @@ let map = L.map('mapid', {
 	layers: [streets]
 });
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
-
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/carlysandler/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
@@ -41,13 +36,44 @@ let myStyle = {
 	weight: 1
 };
 
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
 	// Creating a GeoJSON layer with the retrieved data.
 	L.geoJson(data, {
+		// We turn each feature into a circleMarker on the map.
 		pointToLayer: function (feature, latlng) {
-			return L.marker(latlng);
+			console.log(data);
+			return L.circleMarker(latlng);
+		},
+		// We set the style for each circleMarker using our styleInfo function.
+		style: styleInfo
+		}).addTo(map);
+
+		// This function returns the style data for each of the earthquakes we plot on 
+		// the map. We pass the magnitude of the earthquake into a function
+		// to calacute the radius.
+		function styleInfo(feature) {
+			return {
+				opacity: 1,
+				fillOpacity: 1,
+				fillColor: "#ffae42",
+				color: "#000000",
+				radius: getRadius(feature.properties.mag),
+				stroke: true,
+				weight: 0.5
+			};
 		}
-	}).addTo(map);
-});
+
+		// This function determines the radius of the earthquake marker based on its magnitude
+		// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+		function getRadius(magnitude) {
+			if (magnitude === 0) {
+				return 1;
+			}
+			return magnitude * 4;
+		}
+	});
 
